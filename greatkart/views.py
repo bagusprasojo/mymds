2a5cf1724a9e4a17d137f939243a188158830ad0
bagus ryzen <bagusprasojo@tigaserangkai.co.id>
2022-11-27 06:59:59 +0700

    initiate commit

---
+++ greatkart/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from store.models import Product
+
+def home(request):
+    products = Product.objects.all().filter(is_available=True)
+    print(products)
+
+    for x in products:
+        print(x.product_name)
+
+    context = {
+        'products' : products
+    }
+    return render(request, 'home.html', context)
